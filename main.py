ef0dd97f880058d1a223a8fd16f98314e7529370
mattandersoninf <matt.anderson.inf@gmail.com>
2017-07-19 23:15:09 -0400

    main.py
    main python script to drive web scraping for EV information

---
+++ main.py
@@ -0,0 +1,15 @@
+# bear in mind, writing all of this straight into git because my current laptop is not ideal for 
+# compiling, use desktop once you get new place or find somewhere to run tests
+
+
+# http library that allows python to access webpages
+import requests
+# allows you to analyze the contents of the page (python parsing library), specifically for messy sites
+import beatiful soup 4
+# easy and fast xml and html library in python, same as soup 4 but easier and faster to pick up
+import lxml
+# web automation library, open up the browser and navigate to desired pages
+import selenium
+# DO RESEARCH ON THIS LIBRARY TO UNDERSTAND EXACTLY WHAT IT DOES 07-19-17, sounds useful refer to reseources
+import scrapy
+
