fe7c8f78aab0c30b40d1b3dcd09e43ad5305dd10
Matthew Anderson <matt.anderson.inf@gmail.com>
2017-07-26 23:52:42 -0400

    Update EVCategorizer.py
    preparing to transfer resource links to readme so it doesn't clutter the main structure

--- EVCategorizer.py
+++ EVCategorizer.py
@@ -3,6 +3,9 @@
 # ideal for static webpages
 from lxml import html
 import requests
+# WEB SCRAPING IS LEGAL for most websites
+# so long as one does not scrape at alarming rates or violate an individual webpage's user agreement, have at it!
+# https://www.quora.com/On-which-websites-can-I-do-web-scraping-legally
 # should help with rate limiting
 from ratelimit import *
 import requests
@@ -20,15 +23,17 @@ import scrapy
 # came from this github account
 # https://github.com/NikolaiT/GoogleScraper
 # scrapes google, bing, and other search engines
-# however general consensus that the main 2 libraries required are urllib/urllib2 and bs4
+# however general consensus from multiple sites is that the main 2 libraries required are urllib/urllib2 and bs4
+# for the sake of urlopen and BeautifulSoup, yum soup :)
 
-class EVCategorizer{
+class EVCategorizer:
   def _init_(self):
     # list container for URLs
     self.urlList=[]		
     # dict container for URLs
     self.urlDict= {}
-    # dict containers for the URLs of each individual site 
+    # dict containers for the URLs of each individual site
+    # add more dictionaries if you wish to use googlescraper
     self.googleSearchUrlDict={}
     self.googlePlusUrlDict={}
     self.plugincarsUrlDict={}
@@ -37,6 +42,20 @@ class EVCategorizer{
     # counters for successful hits and failures
     self.successCounter=0
     self.failureCounter=0
+    
+  
+  
+  
+  def __repr__(self):
+		return "EV Category Scraper"
+	def __iter__ (self):
+		for a in urlList:
+			yield a
+	def __getitem__(self,key):
+		for a in self.urlDict.keys():
+			return urlDict[key]
+	def __len__(self) :
+		return len(entryList)
   
 # set up retrieving the contents from each indvidual webpage
 # EV google search webpage content, find another library, other possible solutions include xgoogle and json
@@ -66,4 +85,4 @@ plugincarsSoup = BeautifulSoup(plugincarsData)
 print plugincarsSoup.prettify()
 
 print plugincarsSoup.title.string
-}
+
