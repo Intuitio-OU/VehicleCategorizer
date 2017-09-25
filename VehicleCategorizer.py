fb6259263c7c7f876d1375482115db974d06f50a
matt.anderson.inf <matt.anderson.inf@gmail.com>
2017-09-25 03:19:52 -0400

    1. added more information regarding the scrapy library, seeing that it is it's own api, I have decided to switch to relying on beatuiful soup since I can build my own api. 2. added functionality of using a nested dictionary in order to organize the collected information better. default dict keys are going to be the urls along with properties mentioned in your notepad 3. remeber to be mindful of the visualizations for this data. being able to maintain it well and give it meaning will help alot

--- VehicleCategorizer.py
+++ VehicleCategorizer.py
@@ -18,7 +18,11 @@ from bs4 import BeautifulSoup as soup
 import requests as uReq
 #import selenium
 # DO RESEARCH ON THIS LIBRARY TO UNDERSTAND EXACTLY WHAT IT DOES 07-19-17, sounds useful refer to reseources
+
+# learning curve for scrapy is a bit steeper and it is it's own api, bs4 more prefereable
+# for the sake of developing you're own
 #import scrapy
+
 # in case you want to tap into googlescraper python library, use these lines
 # import sys
 # from GoogleScraper import scrape_with_config, GoogleSearchError
@@ -27,54 +31,68 @@ import requests as uReq
 # http://www.pythonforbeginners.com/beautifulsoup/beautifulsoup-4-python
 
 class VehicleCategorizer:
-  def __init__(self):
-    # list container for URLs
-    self.urlList=[]		
-    # dict container for URLs
-    self.urlDict= {}
-    # dict containers for the URLs of each individual site
-    # add more dictionaries if you wish to use googlescraper
-    #self.googleSearchUrlDict={}
-    #self.googlePlusUrlDict={}
-    self.topspeedUrlDict={}
-    #self.jdpowerUrlDict={}
+    def __init__(self):
+        # list container for URLs
+        self.urlList=[]		
+        # dict container for URLs
+        self.urlDict= {}
+        # dict containers for the URLs of each individual site
+        # add more dictionaries if you wish to use googlescraper
+        #self.googleSearchUrlDict={}
+        #self.googlePlusUrlDict={}
+        self.topspeedUrlDict={}
+        #self.jdpowerUrlDict={}
+        self.plugincars_url_dict=collections.defaultdict(dict)
+        
+        # counters for successful hits and failures
+        self.successCounter=0
+        self.failureCounter=0
+        
+        # get the url of edmunds
+        self.edmunds_url = "https://www.topspeed.com/cars/plugin-cars/ke4486.html"
+        # open the html file of the webpage
+        self.edmundsClient = uReq.get(self.edmunds_url)
+        # get the webpage's html information
+        self.edmunds_soup = soup(self.edmundsClient.content)
+        
+        # get url for plugincars
+        self.plugincars_base_url = "http://www.plugincars.com/cars"
+        
+        # get contents of plugincars.com
+        self.plugincarsClient = uReq.get(self.plugincars_base_url)
+        
+        # get webpage html contents
+        # need the html.parser to get all of the information from this specific page
+        self.plugincars_soup = soup(self.plugincarsClient.content, "html.parser")
+        
+        self.plugincars_url_list = []
+        # plugincars divides up the individual cars into dividers with a mutual class of car-a
+        # that being said, the names of each car is found under h3's text
+        for div in self.plugincars_soup.find_all("div", class_="car-a"):
+            # get href to get the partial url to see the full details of each vehicle
+            self.plugincars_url_list.append(self.plugincars_base_url+str(div.h3.a['href']))
+        for url in self.plugincars_url_list:
+            # need to form a new client in order to get the soup from each url and then
+            curr_plugincars_client = uReq.get(url)
+            # need to get the form the soup to scrape from the individual urls
+            curr_plugincars_soup  = soup(curr_plugincars_client.content, "html.parser")
+            
+            #self.plugincars_url_dict[url]
+        
+        #self.plugincars_page_soup = soup(self.plugincars_page_html, "html.parser")
+        #must always have in order to add in new parameters to the class
+        self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
 
-    # counters for successful hits and failures
-    self.successCounter=0
-    self.failureCounter=0
+    def get_plugincars_names(self):
+        return self.plugincars_names
     
-    # get the url of edmunds
-    self.edmunds_url = "https://www.topspeed.com/cars/plugin-cars/ke4486.html"
-    # open the html file of the webpage
-    self.edmundsClient = uReq.get(self.edmunds_url)
-    # get the webpage's html information
-    self.edmunds_soup = soup(self.edmundsClient.content)
-    
-    # get url for plugincars
-    self.plugincars_url = "http://www.plugincars.com/cars"
-    
-    # get contents of plugincars.com
-    self.plugincarsClient = uReq.get(self.plugincars_url)
-    
-    # get webpage html contents
-    # need the html.parser to get all of the information from this specific page
-    self.plugincars_soup = soup(self.plugincarsClient.content, "html.parser")
-    
-    for atag in  self.plugincars_soup.find_all()
-    
-    # find a pattern for the car names, their all atags but you need to specify the dividers
-    # that contain them uniquely
-    
-    
-    #self.plugincars_page_soup = soup(self.plugincars_page_html, "html.parser")
-    #must always have in order to add in new parameters to the class
-    self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
-    
+        
+        
 # test grabbing information from the page
 vc = VehicleCategorizer()
 #for atag in vc.edmunds_soup.find_all("a"):
     #print(atag.text)
-for a in vc.plugincars_soup.find_all("a"):
-    print(a.text)    
+for i in vc.plugincars_url_list:
+    print(i)
 #print(vc.plugincars_soup)    
     
