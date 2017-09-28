5a510436dbf5534bf9cdd8fb03e3a217fc1910c6
matt.anderson.inf <matt.anderson.inf@gmail.com>
2017-09-28 01:16:21 -0400

    adjusted charge rate to match the same safety condition as battery capacity

--- VehicleCategorizer.py
+++ VehicleCategorizer.py
@@ -87,8 +87,7 @@ class VehicleCategorizer:
             self.plugincars_dict[self.plugincars_car_names_list[i]]['body'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[4].text
             self.plugincars_dict[self.plugincars_car_names_list[i]]['range(mi)'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[6].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[6].text.find(" ")]
             self.plugincars_dict[self.plugincars_car_names_list[i]]['battery_capacity(kWh)'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text.find(" ")] if curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text.find(" ")] != '' else '-1'
-            print("battery_cap", self.plugincars_dict[self.plugincars_car_names_list[i]]['battery_capacity(kWh)'])
-            self.plugincars_dict[self.plugincars_car_names_list[i]]['charge_rate(kW)'] =curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text.find(" ")] # if len(curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})) < 8 else " "
+            self.plugincars_dict[self.plugincars_car_names_list[i]]['charge_rate(kW)'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text.find(" ")] if curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text.find(" ")] != '' else "-1"
         
         
         # create csv file from the data collected
