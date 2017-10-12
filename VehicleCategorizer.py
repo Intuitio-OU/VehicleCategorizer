fc5ca63842f42e3b60cd15af68998e369bdbe587
matt.anderson.inf <matt.anderson.inf@gmail.com>
2017-10-12 05:12:57 -0400

    grequests works with plugincars but is failing when applied to edmunds in the first map command

--- VehicleCategorizer.py
+++ VehicleCategorizer.py
@@ -12,16 +12,14 @@ import collections
 # this will be able to keep track of the data collection rate while scraping
 # from ratelimit import *
 from bs4 import BeautifulSoup as soup
+from bs4 import SoupStrainer as strain
 import requests as uReq
 import grequests as gReq
 from pprint import pprint
 import csv
 import time
-import multiprocessing
 import json
 import re
-
-
 
 class VehicleCategorizer:
     def __init__(self, maxFail = 10):
@@ -66,102 +64,86 @@ class VehicleCategorizer:
     def makePlugincarsUrlList(self):
         # get urls for plugincars, base url is for scraping the urls of each individual car webpage
         # and the other url is for appeniding the url of individual car page
+        self.__initTry__()
         self.plugincars_url = "http://www.plugincars.com"
         self.plugincars_base_url = "http://www.plugincars.com/cars"
         # list to contain all of the urls scraped from the main plugincars page
         self.plugincars_url_list = []
         self.plugincars_car_names_list = []
         
-        #self.makeConnectionToPlugincarsDB()
-        
-        # remember to add try catch for attempting parse the plugincars site
-        #while True:
-        try:
-            # get contents of plugincars.com
-            self.plugincarsClient = uReq.get(self.plugincars_base_url)
-            # get webpage html contents
-            # need the html.parser to get all of the information from this specific page
-            self.plugincars_soup = soup(self.plugincarsClient.content, 'html.parser')
-            # plugincars divides up the individual cars into dividers with a mutual class of car-a
-            # that being said, the names of each car is found under h3's text
-            for div in self.plugincars_soup.findAll("div", {"class" : "car-a"}):
-                # get href to get the partial url to see the full details of each vehicle
-                self.plugincars_url_list.append(self.plugincars_url+div.h3.a['href'])
-                self.plugincars_car_names_list.append(div.h3.a.text)
-            #break
-        # raise awareness if there's an issue connecting to the site
-        except uReq.exceptions.TooManyRedirects:
-            self.failCounter += 1
-            print("There's an issue with connecting to plugincars.com")
-        # if there are any issues that haven;t been accounted for, print it
-        except uReq.exceptions.RequestException as e:
-            self.failCounter += 1
-            print("Unforseen error:",e)
-            """
+        while True:
+            try:
+                # get contents of plugincars.com
+                self.plugincarsClient = uReq.get(self.plugincars_base_url)
+                soupStrain = strain('div',{"class" : "car-a"})
+                # get webpage html contents
+                # need the html.parser to get all of the information from this specific page
+                self.plugincars_soup = soup(self.plugincarsClient.content, 'html.parser', parse_only = soupStrain)
+                # plugincars divides up the individual cars into dividers with a mutual class of car-a
+                # that being said, the names of each car is found under h3's text
+                for div in list(self.plugincars_soup):
+                    # get href to get the partial url to see the full details of each vehicle
+                    self.plugincars_url_list.append(self.plugincars_url+div.h3.a['href'])
+                    self.plugincars_car_names_list.append(div.h3.a.text)
+                break
+            # raise awareness if there's an issue connecting to the site
+            except uReq.exceptions.TooManyRedirects:
+                self.failCounter += 1
+                print("There's an issue with connecting to plugincars.com")
+            # if there are any issues that haven;t been accounted for, print it
+            except uReq.exceptions.RequestException as e:
+                self.failCounter += 1
+                print("Unforseen error:",e)
             # you can keep trying to connect to the site, but if you've exceeded the maxFail count,
             # break the loop and display that there's an issue with makePlugincarsUrlList
             if self.failCounter == self.maxRequests:
-                print('scrapePluginCars error\n')
+                print('makePlugincarsUrlList error\n')
                 break
-            """
             
-    # get all of the information from the lugincars site
+    # get all of the information from the plugincars site
     # while scraping, if a cell returns -1, it means that the information
     # for this specific section was blank    
     def scrapePlugincars(self, filepath = 'plugincars.csv'):
-        # reset the fail counter
-        #self.__initTry__()
+        self.__initTry__()
         self.makePlugincarsUrlList()
-        #self.makeConnectionToPlugincarsDB()
-        #while True:
-        try:
-            rs = (gReq.get(url) for url in self.plugincars_url_list)
-            r = list(gReq.map(rs))
-            for response in r:
-                # need to get the form the soup to scrape from the individual urls
-                curr_plugincars_soup = soup(response.text, "html.parser")
-                # start sorintg out each of the pieces of information from the main soup
-                # and individual car soup
-                # there is an option to put the keys into the
-                curr_car_dict = self.plugincars_dict[response.url]
-                curr_car_dict['name'] = curr_plugincars_soup.find('title').text[0:curr_plugincars_soup.find('title').text.index(' |')]
-                curr_car_dict['make'] = curr_plugincars_soup.find("h3", {"class" : "vehicle-stats-title"}).text[0:curr_plugincars_soup.find("h3", class_="vehicle-stats-title").text.find(" ")]
-                curr_car_dict['model'] = curr_plugincars_soup.find("h3", {"class" : "vehicle-stats-title"}).text[curr_plugincars_soup.find("h3", class_="vehicle-stats-title").text.find(" ")+1:curr_plugincars_soup.find("h3", class_="vehicle-stats-title").text.find(" specifications")]
-                curr_car_dict['base_msrp($)'] = ''.join(x for x in curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[1].text if x.isdigit()) if curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[1].text != '' else '-1'
-                curr_car_dict['tech'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[3].text
-                curr_car_dict['body'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[4].text
-                curr_car_dict['range(mi)'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[6].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[6].text.find(" ")]
-                curr_car_dict['battery_capacity(kWh)'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text.find(" ")] if curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text.find(" ")] != '' else '-1'
-                curr_car_dict['charge_rate(kW)'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text.find(" ")] if curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text.find(" ")] != '' else "-1"
-            # create csv file from the data collected
-            # intialize a list for the parameters that classify your values        
-            fieldnames = list(list(self.plugincars_dict.values())[0].keys())
-            # write the collected information in the dictionary for plugincars into a csv file labeled plugincars.csv
-            with open(filepath,'w', newline ='') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames)
-                writer.writeheader()
-                for key in self.plugincars_dict: writer.writerow({field: self.plugincars_dict[key].get(field) or key for field in fieldnames})
-            csvfile.close()
-            #break
-        # raise awareness if there's an issue connecting to the site
-        except uReq.exceptions.TooManyRedirects:
-            self.failCounter += 1
-            print("There's an issue with connecting to plugincars.com")
-        # if there are any issues that haven;t been accounted for, print it
-        except uReq.exceptions.RequestException as e:
-            self.failCounter += 1
-            print("Unforseen error:",e)
-        except KeyError or AttributeError:
-            self.failCounter += 1
-            print("You're trying to grab an attribute that an xml element does not have.")
-        except csv.Error:
-            self.failCounter += 1
-            print("There's problem when writing to the csv.")
-            """
+        while True:
+            try:
+                rs = (gReq.get(url) for url in self.plugincars_url_list)
+                r = list(gReq.map(rs))
+                for response in r:
+                    curr_plugincars_soup = soup(response.text, "html.parser")
+                    curr_car_dict = self.plugincars_dict[response.url]
+                    curr_car_dict['name'] = curr_plugincars_soup.find('h1').a.text
+                    curr_car_dict['make'] = curr_plugincars_soup.find("h3", {"class" : "vehicle-stats-title"}).text[0:curr_plugincars_soup.find("h3", class_="vehicle-stats-title").text.find(" ")]
+                    curr_car_dict['model'] = curr_plugincars_soup.find("h3", {"class" : "vehicle-stats-title"}).text[curr_plugincars_soup.find("h3", class_="vehicle-stats-title").text.find(" ")+1:curr_plugincars_soup.find("h3", class_="vehicle-stats-title").text.find(" specifications")]
+                    curr_car_dict['base_msrp($)'] = ''.join(x for x in curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[1].text if x.isdigit()) if curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[1].text != '' else '-1'
+                    curr_car_dict['tech'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[3].text
+                    curr_car_dict['body'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[4].text
+                    curr_car_dict['range(mi)'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[6].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[6].text.find(" ")]
+                    curr_car_dict['battery_capacity(kWh)'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text.find(" ")] if curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[7].text.find(" ")] != '' else '-1'
+                    curr_car_dict['charge_rate(kW)'] = curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text.find(" ")] if curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text[0:curr_plugincars_soup.find_all("td", {"class" : "vehicle-stats-data"})[8].text.find(" ")] != '' else "-1"
+                fieldnames = list(list(self.plugincars_dict.values())[0].keys())
+                with open(filepath,'w', newline ='') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames)
+                    writer.writeheader()
+                    for key in self.plugincars_dict: writer.writerow({field: self.plugincars_dict[key].get(field) or key for field in fieldnames})
+                csvfile.close()
+                break
+            except uReq.exceptions.TooManyRedirects:
+                self.failCounter += 1
+                print("There's an issue with connecting to plugincars.com")
+            except uReq.exceptions.RequestException as e:
+                self.failCounter += 1
+                print("Unforseen error:",e)
+            except KeyError or AttributeError:
+                self.failCounter += 1
+                print("You're trying to grab an attribute that an xml element does not have.")
+            except csv.Error:
+                self.failCounter += 1
+                print("There's problem when writing to the csv.")
             if self.failCounter == self.maxFail:
                 print('scrapePluginCars error\n')
                 break
-            """
     
     def printPlugincarsDict(self):
         pprint(dict(self.plugincars_dict))
@@ -172,57 +154,54 @@ class VehicleCategorizer:
         self.edmunds_url_set = set()
         self.edmunds_car_names_list = []
         self.__initTry__()
-        try:
-            self.edmundsClient = uReq.get(self.edmunds_url)
-            self.edmundsSoup = soup(self.edmundsClient.content, 'html.parser')
-            # this is the cleanest solution because it is possible that the site can add more car makes later but stable for extracting data in the mean time
-            self.edmundsLinks = self.edmundsSoup.find_all('a')[7:113]
-            for a in self.edmundsLinks:
-                #print('\nedmundsLinks\n')
-                testMakeLink = a['href'][1:]
-                testMakeClient = uReq.get(self.edmunds_url+testMakeLink)
-                testMakeSoup = soup(testMakeClient.content, 'html.parser')
-                testYearLink = testMakeSoup.find_all('div',{'class' : "card-img"})
-                # test progress at this step
-                # print(testYearLink)
-                for yearLink in testYearLink:
-                    # in case the car image isn't available with it's atag skip it
-                    if yearLink.a == None:
-                        continue
-                    tYearClient = uReq.get(self.edmunds_url+yearLink.a['href'][1:])
-                    tYearSoup = soup(tYearClient.content, 'html.parser')
-                    # unstable for in later builds because the format can change
-                    # these classes are randomly generated and edmunds obviously doesn't want people
-                    # scaping without using their own api but it is possible and stable for the
-                    # current build
-                    li_tYearSoup = list(set(tYearSoup.find_all('li', {'class' : 'medium'})))
-                    #pprint.pprint(li_tYearSoup)
-                    tYearLink =[]
-                    for li in li_tYearSoup:
-                        #print('li_tYearSoup\n')
-                        #print('li_tYearLink raw:', li.a['href'])
-                        if li.a['href'].find('used/') != -1:
-                            tYearLink.append(li.a['href'][1:])
-                        else:
-                            slCount = []
-                            index = 0
-                            while index < len(li.a['href']):
-                                index = li.a['href'].find('/', index)
-                                if index == -1:
-                                    break
-                                slCount.append(index)
-                                index += 1
-                            if len(slCount) == 5 and (li.a['href'].find('review') == -1 and li.a['href'].find('features-specs') ==-1 and li.a['href'].find('consumer-reviews') and li.a['href'].find('deals') == -1):
-                                tYearLink.append(li.a['href'][1:])
-                    for link in tYearLink:
-                        self.edmunds_url_set.add(self.edmunds_url+link)
-        except TypeError:
-            print("You're hitting an object that is empty.")
-            pass
-        except Exception as e:
-            # self.failCounter += 1
-            #if self.failCounter >= self.maxFail:
-            print("There is something wrong:", e)
+        #while True:
+        #try:
+        self.edmundsClient = uReq.get(self.edmunds_url)
+        edmundsStrain = strain('a')
+        edmundsSoup = list(soup(self.edmundsClient.content, 'html.parser', parse_only=edmundsStrain))[7:51]
+        for i in range(len(edmundsSoup)): edmundsSoup[i] = self.edmunds_url + edmundsSoup[i]['href'][1:]
+        # this is the cleanest solution because it is possible that the site can add more car makes later but stable for extracting data in the mean time
+        edmundsMakeRS = (gReq(url) for url in edmundsSoup)
+        edmundsMakeResponses = list(gReq.map(edmundsMakeRS))
+        print(edmundsMakeResponses)
+        for makeResponse in edmundsMakeResponses:
+            print('made it to secondary loop')
+            edmundsMakeStrain = strain('div',{'class' : "card-container"})
+            edmundsMakeSoup = list(soup(makeResponse.text, 'html.parser', parse_only = edmundsMakeStrain))
+            edmundsYearRS = (gReq(self.edmunds_url+div.a['href'][1:]) for div in edmundsMakeSoup)
+            edmundsYearResponses = gReq.map(edmundsYearRS)
+            for yearResponse in edmundsYearResponses:
+                edmundsYearStrain = strain('li', {'class' : 'medium'})
+                edmundsYearSoup = list(set(soup(yearResponse.text, 'html.parser', parse_only = edmundsYearStrain)))
+                #pprint.pprint(edmundsYearSoup)
+                for li in edmundsYearSoup:
+                    #print('li_tYearSoup\n')
+                    #print('li_tYearLink raw:', li.a['href'])
+                    if li.a['href'].find('used/') != -1:
+                        self.edmunds_url_set.add(self.edmunds_url+li.a['href'][1:])
+                    else:
+                        slCount = []
+                        index = 0
+                        while index < len(li.a['href']):
+                            index = li.a['href'].find('/', index)
+                            if index == -1:
+                                break
+                            slCount.append(index)
+                            index += 1
+                        if len(slCount) == 5 and (li.a['href'].find('review') == -1 and li.a['href'].find('features-specs') ==-1 and li.a['href'].find('consumer-reviews') and li.a['href'].find('deals') == -1):
+                            self.edmunds_url_set.add(self.edmunds_url+li.a['href'][1:])
+                        #break
+            """
+            except TypeError as e:
+                print("You're hitting an object that is empty.")
+                self.failCounter += 1
+                if self.failCounter >= self.maxFail:
+                    print("There is something wrong:", e)
+            except Exception as e:
+                self.failCounter += 1
+                if self.failCounter >= self.maxFail:
+                    print("There is something wrong:", e)
+            """
         #pprint.pprint(self.edmunds_url_set)
             
     def scrapeEdmunds(self):
@@ -232,11 +211,13 @@ class VehicleCategorizer:
             print(url)
     
         
-    #def fillPlugincarsDB(self):
-        # fill mongo database with the information collected on each vehicle
-    
-    """    
     # reset the failCounter so that you keep attempting to scrape the site until you get a hit
     def __initTry__(self):
         self.failCounter = 0
-    """    
+       
+"""
+testReq = uReq.get('https://www.edmunds.com/aston-martin/')
+testStrain = strain('div', {'class':'card-container'})
+print(list(soup(testReq.content, 'html.parser', parse_only = testStrain))[0].a)
+print('soup len', len(list(soup(testReq.content, 'html.parser', parse_only = testStrain))))
+"""
--- test_VehicleCategorizerTestCase.py
+++ test_VehicleCategorizerTestCase.py
@@ -19,7 +19,7 @@ class VehicleCategorizerTestCase(unittest.TestCase):
         except ImportError as err: print("Check that you imported the VehicleCategorizer class and that it's in the same folder as this test script.")
      
     # test the information scraped from the plugincars website    
-    #@unittest.skip("Skip plugincars for this test.")
+    @unittest.skip("Skip plugincars for this test.")
     def test_plugincars(self):
         try:
             start = time.time()
@@ -33,7 +33,7 @@ class VehicleCategorizerTestCase(unittest.TestCase):
         except IndexError: print("You're over indexing in the amount of variable that are available in the car name list.")
      
     # test the information collected from the edmunds site (still in progress)
-    @unittest.skip("Skip edmunds for this test.")
+    #@unittest.skip("Skip edmunds for this test.")
     def test_edmunds(self):
         try:
             start = time.time()
